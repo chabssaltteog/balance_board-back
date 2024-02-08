91baac18fd7ffb8a3ce9669e3949f8218d6c9b13
HeoJunHyuk <gj7926@naver.com>
2024-02-08 16:44:30 +0900

    Initial commit

---
+++ src/main/java/chabssaltteog/balance_board/BalanceBoardApplication.java
@@ -0,0 +1,13 @@
+package chabssaltteog.balance_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceBoardApplication.class, args);
+	}
+
+}
