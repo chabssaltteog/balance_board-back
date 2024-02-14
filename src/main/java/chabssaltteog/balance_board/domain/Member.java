75143127ea6c3e0ab325f5542d0daef2f0f61c3f
HeoJunHyuk <gj7926@naver.com>
2024-02-14 23:55:48 +0900

    Feat findByName

--- src/main/java/chabssaltteog/balance_board/domain/Member.java
+++ src/main/java/chabssaltteog/balance_board/domain/Member.java
@@ -1,10 +1,17 @@
 package chabssaltteog.balance_board.domain;
 
 
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "user")
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private long userId;        //pk
 
     private String email;       //googleProfile 클래스로 빼서 객체 추가해도 됨
@@ -17,6 +24,7 @@ public class Member {
 
     private String gender;      //사용자 입력값
 
+    @Column(name = "image_url")
     private String imageUrl;    //프로필 사진
 
     private LocalDateTime created;  //계정 생성 날짜
--- src/main/java/chabssaltteog/balance_board/repository/MemberRepository.java
+++ src/main/java/chabssaltteog/balance_board/repository/MemberRepository.java
@@ -1,7 +1,12 @@
 package chabssaltteog.balance_board.repository;
 
+import chabssaltteog.balance_board.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    public Optional<Member> findByName(String name);    // 최초 로그인 여부를 체크하기 위함
 }
