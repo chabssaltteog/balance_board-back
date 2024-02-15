bdc152b5436c70a9a053fdf3acf26cb4cf27ba15
JunHyuk <gj7926@naver.com>
2024-02-15 13:39:41 +0900

    Docs : build.gradle

--- src/main/java/chabssaltteog/balance_board/repository/MemberRepository.java
+++ src/main/java/chabssaltteog/balance_board/repository/MemberRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    public Optional<Member> findByName(String nickname);    // 닉네임 중복 확인
+    public Optional<Member> findByNickname(String nickname);    // 닉네임 중복 확인
 
     public Optional<Member> findByProviderId(Long googleId);
 }
