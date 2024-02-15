7c8e6e88acea72145c77e8aaf7bacfd192218898
JunHyuk <gj7926@naver.com>
2024-02-15 15:28:15 +0900

    Feat : SessionMember add

--- src/main/java/chabssaltteog/balance_board/domain/SessionMember.java
+++ src/main/java/chabssaltteog/balance_board/domain/SessionMember.java
@@ -5,8 +5,20 @@ import lombok.Getter;
 import java.io.Serializable;
 
 @Getter
-public class SessionMember implements Serializable {
+public class SessionMember implements Serializable {    // 인증된 사용자 정보만 필요
     private String name;
     private String email;
     private String imageUrl;
+    private String nickname;
+    private int age;
+    private String gender;
+
+    public SessionMember(Member member) {
+        this.name = member.getName();
+        this.email = member.getEmail();
+        this.imageUrl = member.getImageUrl();
+        this.nickname = member.getNickname();
+        this.age = member.getAge();
+        this.gender = member.getGender();
+    }
 }
