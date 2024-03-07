919890655def588106db622b161fd60a8edbcf8c
HeoJunHyuk <gj7926@naver.com>
2024-03-07 20:49:39 +0900

    Style : swagger ui modify

--- src/main/java/chabssaltteog/balance_board/dto/post/CommentDeleteDTO.java
+++ src/main/java/chabssaltteog/balance_board/dto/post/CommentDeleteDTO.java
@@ -7,10 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Schema(title = "댓글 삭제 requestDTO")
+@Schema(title = "COM_REQ_02 : 댓글 삭제 요청 DTO")
 public class CommentDeleteDTO {
 
+    @Schema(description = "삭제할 댓글 ID", example = "7")
     private Long commentId;
 
+    @Schema(description = "사용자 ID", example = "23")
     private Long currentUserId;
 }
