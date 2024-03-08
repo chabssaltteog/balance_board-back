7b8eb57216c8a3c63cd8a5f3f22e3d5dbb15af62
minje <k27022000@gmail.com>
2024-03-08 21:14:23 +0900

    test 서버 배포용

--- src/main/java/chabssaltteog/balance_board/api/MainApiController.java
+++ src/main/java/chabssaltteog/balance_board/api/MainApiController.java
@@ -24,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
