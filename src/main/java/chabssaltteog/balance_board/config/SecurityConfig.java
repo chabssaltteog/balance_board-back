dd89b1ac3b6b891f6e17fc05621ae82d49ee8b72
HeoJunHyuk <gj7926@naver.com>
2024-03-25 17:04:02 +0900

    Refactor : cors modify

--- src/main/java/chabssaltteog/balance_board/config/SecurityConfig.java
+++ src/main/java/chabssaltteog/balance_board/config/SecurityConfig.java
@@ -71,10 +71,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://52.78.127.145:8080", "http://127.0.0.1:3000", "http://localhost:3000", "http://localhost:8080",
-                "https://balance-board-front-git-develop-chabssaltteog.vercel.app", "https://balance-board-front-1wyd.vercel.app",
-                "https://balanceboard.swygbro.com"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
